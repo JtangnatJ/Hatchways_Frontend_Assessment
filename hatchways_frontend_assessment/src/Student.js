3154f44c96d61a17004286528c9f09459ebe4f51
Jacky Tang <jackytang74@gmail.com>
2021-07-20 17:25:55 -0700

    edit formatting to match example, Part 1 complete

--- hatchways_frontend_assessment/src/Student.js
+++ hatchways_frontend_assessment/src/Student.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const Student = (props) => (
-    <div id="studentInfo">
-        <img id="pic" src={props.student.pic} alt="Profile Pic"/>
-        <div id="firstName">{props.student.firstName}</div>
-        <div id="lastName">{props.student.LastName}</div>
-        <div id="city">{props.student.city}</div>
-        <div id="company">{props.student.company}</div>
-        <div id="email">{props.student.email}</div>
-        <div id="skill">{props.student.skill}</div>
-        <div id="grades">{props.student.grades}</div>
-    </div>
-);
+const Student = (props) => {
+    const student = props.student;
+    const grades = student.grades;
+    const total = grades.reduce((a, b) => parseInt(a) + parseInt(b));
+    const average = total / grades.length;
+
+    return(
+        <div id="studentInfo">
+            <img id="pic" src={student.pic} alt="Profile Pic"/>
+            <div id ="name">{student.firstName} {student.lastName}</div>
+            <div id="email">Email: {student.email}</div>
+            <div id="company"> Company: {student.company}</div>
+            <div id="skill">Skill: {student.skill}</div>
+            <div id="average">Average: {average}%</div>
+        </div>
+    )
+};
 
 export default Student;
