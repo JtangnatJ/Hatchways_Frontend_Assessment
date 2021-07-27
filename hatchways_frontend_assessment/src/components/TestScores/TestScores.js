d8157a0de918a016aa743ed99414b66125273ce8
Jacky Tang <jackytang74@gmail.com>
2021-07-26 21:39:36 -0700

    formatted code

--- hatchways_frontend_assessment/src/components/TestScores/TestScores.js
+++ hatchways_frontend_assessment/src/components/TestScores/TestScores.js
@@ -1,24 +1,29 @@
 import React, { useEffect } from "react";
-import"./TestScores.css";
+import "./TestScores.css";
 
 const TestScores = (props) => {
 	const [displayed, setDisplayed] = React.useState(null);
 
-    useEffect(() => {
-        setDisplayed(props.showScores);
-    }, [props.showScores]);
+	useEffect(() => {
+		setDisplayed(props.showScores);
+	}, [props.showScores]);
 
-	return <div className="testScores">{
-        displayed ? 
-        props.grades.map((grade, index) => (
-            <div className="testGrade">
-                <div>Test {index + 1}:</div>
-                <div>{grade}%</div>
-            </div>
-        ))
-        : 
-        <></>}
-        </div>;
+	return (
+		<div>
+			{displayed ? (
+				<div className="testScores">
+					{props.grades.map((grade, index) => (
+						<div className="testGrade">
+							<div>Test {index + 1}:</div>
+							<div>{grade}%</div>
+						</div>
+					))}
+				</div>
+			) : (
+				<></>
+			)}
+		</div>
+	);
 };
 
 export default TestScores;
